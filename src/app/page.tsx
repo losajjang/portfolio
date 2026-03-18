34b6714cd42f57b79403fa2f9212a1b9520d9987
losajjang <losajjang@naver.com>
2026-03-18 18:28:24 +0900

    [feat] 디자인 시스템 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,15 @@
+import clsx from "clsx";
 
 export default function Home() {
-
-  
-
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <h1 className="text-2xl px-6">
-        안녕하세요
-        <br />
-        프론트엔드 개발자 박재민입니다.
-      </h1>
+    <div className="flex min-h-screen items-center justify-center">
+      <div>
+        <h1 className="typo-display1 px-6 text-gray-gray_80">
+          안녕하세요
+          <br />
+          프론트엔드 개발자 박재민입니다.
+        </h1>
+      </div>
     </div>
   );
 }
