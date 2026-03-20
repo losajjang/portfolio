908e6f20b7b21f32d4db79093e274c6a2983af7c
losajjang <losajjang@naver.com>
2026-03-20 19:52:01 +0900

    [fix] 화면 진입시 텍스트 타이핑 애니메이션 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,71 @@
+"use client";
 import clsx from "clsx";
 import { Abilities, Career, Education, Intro, Links } from "./components";
+import { useEffect, useState } from "react";
+import useTypingText from "@/utils/hooks/useTypingText";
 
 export default function Home() {
+  const typedText = useTypingText({
+    text: "안녕하세요\n프론트엔드 개발자 박재민입니다.",
+    speed: 100,
+    startDelay: 500,
+  });
+
+  const [isHelloTextMoved, setIsHelloTextMoved] = useState(false);
+  const [showContent, setShowContent] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsHelloTextMoved(true);
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  useEffect(() => {
+    if (isHelloTextMoved) {
+      const timer = setTimeout(() => {
+        setShowContent(true);
+      }, 500);
+
+      return () => clearTimeout(timer);
+    }
+  }, [isHelloTextMoved]);
+
   return (
-    <section className="flex flex-col min-h-screen items-center justify-center">
-      <div>
-        <h1 className="typo-display1 px-6 text-gray-gray_80">
-          안녕하세요
-          <br />
-          프론트엔드 개발자 박재민입니다.
+    <section
+      className={clsx(
+        "relative",
+        "w-full h-screen pl-80 pt-4",
+        isHelloTextMoved ? "flex-row" : "flex-col",
+      )}
+    >
+      <div
+        className={clsx(
+          "flex items-center justify-center",
+          isHelloTextMoved ? "w-fit" : "w-full",
+        )}
+      >
+        <h1
+          className={clsx(
+            "whitespace-pre",
+            "text-gray-gray_80",
+            "transition-all duration-500 ease-in-out",
+            isHelloTextMoved
+              ? "absolute top-4 left-4 translate-x-0 translate-y-0 typo-title2"
+              : "absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 typo-display1",
+          )}
+        >
+          {typedText}
+          {!isHelloTextMoved && <span className="animate-blink">|</span>}
         </h1>
       </div>
-      <div className="hidden">
+      <div
+        className={clsx(
+          "transition-all duration-500 ease-in-out",
+          showContent ? "opacity-100 w-full h-full" : "opacity-0",
+        )}
+      >
         <article>
           <Intro />
         </article>
