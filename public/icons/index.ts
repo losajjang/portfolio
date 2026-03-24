a427d8a7e5905f13f21e1bef09a3b703f718b408
losajjang <losajjang@naver.com>
2026-03-24 21:53:55 +0900

    [fix] 아이콘 추가

---
+++ public/icons/index.ts
@@ -0,0 +1,17 @@
+import javascriptIcon from "./JavaScript_logo.svg";
+import typescriptIcon from "./Typescript_logo.svg";
+import nextjsIcon from "./Next_js_logo.svg";
+import bitbucketIcon from "./Bitbucket_logo.svg";
+import notionIcon from "./Notion_logo.svg";
+import figmaIcon from "./Figma_logo.svg";
+import slackIcon from "./Slack_logo.svg";
+
+export {
+  javascriptIcon,
+  typescriptIcon,
+  nextjsIcon,
+  bitbucketIcon,
+  notionIcon,
+  figmaIcon,
+  slackIcon,
+};
