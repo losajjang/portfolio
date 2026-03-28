7d92d096d1a1e3488d7c205c038f175b813dbef7
losajjang <losajjang@naver.com>
2026-03-28 18:43:36 +0900

    [fix] NEXT_PUBLIC_IS_PROD 삭제. NODE_ENV로 사용하도록 변경

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,8 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NEXT_PUBLIC_IS_PROD === "true";
+const isProd = process.env.NODE_ENV === "production";
 
-const nextConfig: NextConfig = { 
+const nextConfig: NextConfig = {
   output: "export",
   trailingSlash: true,
   basePath: isProd ? "/portfolio" : undefined,
